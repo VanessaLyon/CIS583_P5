7ea66cbee6f80f8918c7bd488688e8409d0e08f7
VanessaLyon <62832343+VanessaLyon@users.noreply.github.com>
2024-03-03 21:27:52 -0500

    Update bridge.py
    startblock

--- bridge.py
+++ bridge.py
@@ -318,7 +318,8 @@ def scanBlocks(chain):
     # Correctly calculate start_block and end_block
     current_block = w3.eth.block_number
 	
-    start_block = max(1, current_block - 20)  # Ensure start_block is not negative
+    start_block = 300000000  # Ensure start_block is not negative
+
     end_block = current_block  # Use the current block number as end_block
 
     if chain == 'source':
